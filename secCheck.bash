66c4774e34f92fba7bf3a4d02401f645aa337a05
jaredwhichard <47866693+jaredwhichard@users.noreply.github.com>
2020-09-17 00:14:26 -0400

    Update secCheck.bash

--- secCheck.bash
+++ secCheck.bash
@@ -1,4 +1,16 @@
 # A simple script for unix to check account security
+# Requres root access
+echo "Check for users without a password, should return nothing"
+awk -F: '( $2 == "" ) { print } ' /etc/shadow 
+  
+echo "Check for users with a uid of 0, root should be the only response"
+awk -F: '( $3 == "0" ) { print } ' /etc/shadow
 
-echo "Check for users without a password"
-  awk -F: '( $2 == "" ) { print } ' /etc/shadow 
+echo "Check the enabled services"
+systemctl list-unit-files --type=service |grep enabled
+
+echo "Open Network Sockets"
+ss -tulpn
+
+echo "Further infor on active services"
+lsof -i -n
